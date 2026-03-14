d3197d603784bb5f377ca48a4c763306c0ba7694
Mohamed Fareedh Mouzamjah <tasrifmouzam2958@gmail.com>
2026-03-14 13:31:25 +0530

    fareedh | recommendation Pipeline with package handling

--- backend/src/recommender/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes the recommender directory a Python package
---
+++ backend/src/recommender/recommendation_pipeline/__init__.py
@@ -0,0 +1,3 @@
+from recommender.recommendation_pipeline.recommendation_pipeline import recommend_food
+
+__all__ = ["recommend_food"]
